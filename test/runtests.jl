9603de8f61e79743525b90b1133b673e56404e55
Basazin <belhub@uw.edu>
2020-01-03 12:53:54 -0800

    add the my_f func
    my_f is a linear func just for demo

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Myjuliapacr
 using Test
-
+@test my_f(2,1) == 7
+@test my_f(2,3) == 13
 @testset "Myjuliapacr.jl" begin
-    # Write your own tests here.
+    # 2x + 3y
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
 end
